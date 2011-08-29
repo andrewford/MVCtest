5dd82e06e2b9f08751ee05167345133cb4b26e4b
Andrew Ford <aford@scrippsnetworks.com>
2011-08-29 14:35:25 -0400

    Initial Add

---
+++ mvcOne/Controllers/testController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace mvcOne.Controllers
+{
+    public class testController : Controller
+    {
+        //
+        // GET: /test/
+
+        public ActionResult Index()
+        {
+            ViewBag.FirstThing = "hi there";
+
+            return View();
+        }
+
+    }
+}
